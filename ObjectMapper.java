b00c5b602bf0957fc7df049258e8cf5bfffb76f9
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2020-04-16 11:10:46 -0700

    stores and supplies location of data segments

---
+++ ObjectMapper.java
@@ -0,0 +1,213 @@
+import java.util.Hashtable;
+
+public class ObjectMapper
+{
+	public static Hashtable getObjectTable()
+	{
+		Hashtable table = new Hashtable();
+		
+		//Unverified.
+		int key        = 1;
+		int[][] object = new int[2][2];
+		object[0][0]   = 5140;
+		object[0][1]   = 5785;
+		object[1][0]   = 18745;
+		object[1][1]   = 19380;
+		table.put(key,  object);
+		
+		//Unverified.
+	    key          = 2;
+		object       = new int[4][2];
+		object[0][0] = 32650;
+		object[0][1] = 33285;
+		object[1][0] = 46175;
+		object[1][1] = 46810;
+		object[2][0] = 60215;
+		object[2][1] = 60845;
+		object[3][0] = 73655;
+		object[3][1] = 74295;
+		table.put(key,  object);
+		
+		//Unverified.
+		key          = 3;
+		object       = new int[2][2];
+		object[0][0] = 6000;
+		object[0][1] = 6430;
+		object[1][0] = 18100;
+		object[1][1] = 18530;
+		table.put(key,  object);
+		
+
+		//Unverified.
+		key          = 4;
+		object       = new int[3][2];
+		object[0][0] = 60420;
+		object[0][1] = 61055;
+		object[1][0] = 73435;
+		object[1][1] = 74085;
+		object[1][0] = 88090;
+		object[1][1] = 88735;
+		table.put(key,  object);
+		
+		//Unverified.
+		key          = 5;
+		object       = new int[3][2];
+		object[0][0] = 45740;
+		object[0][1] = 46175;
+		object[1][0] = 60845;
+		object[1][1] = 61265;
+		object[1][0] = 73180;
+		object[1][1] = 73655;
+		table.put(key,  object);
+		
+		//Unverified.
+		key          = 6;
+		object       = new int[3][2];
+		object[0][0] = 17890;
+		object[0][1] = 18530;
+		object[1][0] = 33490;
+		object[1][1] = 34100;
+		object[1][0] = 45310;
+		object[1][1] = 45960;
+		table.put(key,  object);		
+
+		//Unverified.
+		key          = 7;
+		object       = new int[3][2];
+		object[0][0] = 170615;
+		object[0][1] = 171250;
+		object[1][0] = 184305;
+		object[1][1] = 184950;
+		object[1][0] = 198215;
+		object[1][1] = 198875;
+		table.put(key,  object);
+		
+		//Probable.
+		key          = 8;
+		object       = new int[3][2];
+		object[0][0] = 127850;
+		object[0][1] = 129090;
+		object[1][0] = 143605;
+		object[1][1] = 144870;
+		object[1][0] = 155625;
+		object[1][1] = 156950;
+		table.put(key,  object);
+		
+		//Probable.
+	    key          = 9;
+		object       = new int[4][2];
+		object[0][0] = 100845;
+		object[0][1] = 101505;
+		object[1][0] = 115965;
+		object[1][1] = 116535;
+		object[2][0] = 127585;
+		object[2][1] = 128865;
+		object[3][0] = 143820;
+		object[3][1] = 145080;
+		table.put(key,  object);
+		
+		
+		//Probable.
+		key          = 15;
+		object       = new int[3][2];
+		object[0][0] = 116535;
+		object[0][1] = 116955;
+		object[1][0] = 127850;
+		object[1][1] = 128280;
+		object[2][0] = 144485;
+		object[2][1] = 144870;
+		table.put(key,  object);		
+		
+		//Probable.  Something wrong here--objects 15 and 16 have same info.
+		key          = 16;
+		object       = new int[3][2];
+		object[0][0] = 116395;
+		object[0][1] = 116955;
+		object[1][0] = 127850;
+		object[1][1] = 128280;
+		object[2][0] = 144485;
+		object[2][1] = 144870;
+		table.put(key,  object);
+
+		//Probable.
+	    key          = 21;
+		object       = new int[5][2];
+		object[0][0] = 226375;
+		object[0][1] = 227780;
+		object[1][0] = 237900;
+		object[1][1] = 239390;
+		object[2][0] = 253560;
+		object[2][1] = 254810;
+		object[3][0] = 265450;
+		object[3][1] = 266940;
+		object[4][0] = 281030;
+		object[4][1] = 282565;
+		table.put(key,  object);
+
+		//Probable.
+	    key          = 22;
+		object       = new int[6][2];
+		object[0][0] = 183200;
+		object[0][1] = 183860;
+		object[1][0] = 199250;
+		object[1][1] = 199870;
+		object[2][0] = 210905;
+		object[2][1] = 211550;
+		object[3][0] = 226695;
+		object[3][1] = 227120;
+		object[4][0] = 238000;
+		object[4][1] = 239020;
+		object[5][0] = 253765;
+		object[5][1] = 254100;
+		table.put(key,  object);
+
+		//Probable.
+		key          = 29;
+		object       = new int[3][2];
+		object[0][0] = 226905;
+		object[0][1] = 227780;
+		object[1][0] = 237900;
+		object[1][1] = 238760;
+		object[2][0] = 253975;
+		object[2][1] = 254810;
+		table.put(key,  object);
+		
+		//Probable.
+		key          = 35;
+		object       = new int[3][2];
+		object[0][0] = 363580;
+		object[0][1] = 364605;
+		object[1][0] = 375490;
+		object[1][1] = 376555;
+		object[2][0] = 391070;
+		object[2][1] = 392100;
+		table.put(key,  object);
+		
+		//Probable.
+	    key          = 38;
+		object       = new int[4][2];
+		object[0][0] = 227955;
+		object[0][1] = 228765;
+		object[1][0] = 236760;
+		object[1][1] = 237675;
+		object[2][0] = 255015;
+		object[2][1] = 255850;
+		object[3][0] = 264355;
+		object[3][1] = 264580;
+		table.put(key,  object);	
+		
+		//Probable.
+		key          = 51;
+		object       = new int[3][2];
+		object[0][0] = 362355;
+		object[0][1] = 363170;
+		object[1][0] = 376970;
+		object[1][1] = 377810;
+		object[2][0] = 389335;
+		object[2][1] = 390220;
+		table.put(key,  object);		
+		
+		return table;
+		
+	}
+}
